3c74d81aedec77ed662e524958695383088ea242
Yulai202020 <ngmyulai@gmail.com>
2024-06-08 16:55:41 +0400

    first commit

---
+++ c.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <netdb.h>
+
+#include <ares.h>
+#include <ares_dns.h>
+#include <arpa/inet.h>
+#include <arpa/nameser.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+std::vector<std::string> ips = {};
+std::vector<std::string> rederect = {};
+
+
+std::vector<std::string> split(const std::string &str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    
+    for (char ch : str) {
+        if (ch == delimiter) {
+            if (!token.empty()) {
+                tokens.push_back(token);
+                token.clear();
+            }
+        } else {
+            token += ch;
+        }
+    }
+    
+    if (!token.empty()) {
+        tokens.push_back(token);
+    }
+    
+    return tokens;
+}
+
+// Callback function to handle the DNS response
+void callback(void* arg, int status, int timeouts, unsigned char* abuf, int alen) {
+    if (status != ARES_SUCCESS) {
+        std::cerr << "Failed to look up SPF record: " << ares_strerror(status) << std::endl;
+        return;
+    }
+
+    struct ares_txt_reply* txt_out = nullptr;
+    status = ares_parse_txt_reply(abuf, alen, &txt_out);
+    if (status != ARES_SUCCESS) {
+        std::cerr << "Failed to parse TXT record: " << ares_strerror(status) << std::endl;
+        return;
+    }
+
+    struct ares_txt_reply* txt_curr = txt_out;
+    std::vector<std::vector<std::string>> end;
+    std::vector<std::string> result;
+    while (txt_curr) {
+        std::string txt_record((char*)txt_curr->txt, txt_curr->length);
+        if (txt_record.find("v=spf1") != std::string::npos) {
+            std::vector<std::string> tmp = split(txt_record, ' ');
+            result.insert(result.end(), tmp.begin(), tmp.end());
+        }
+        txt_curr = txt_curr->next;
+    }
+
+    for (int i = 0; i < result.size(); i++) {
+        std::vector<std::string> tmp = split(result[i], '=');
+        std::cout << tmp[0] << "\n";
+        if (tmp.size() > 1) {
+            end.push_back(tmp);
+        } else {
+            continue;
+        }
+    }
+
+    for (int i = 0; i < end.size(); i++) {
+        std::cout << end[i][0] << " " << end[i][1] << "\n";
+        if (end[i][0] == "redirect" || end[i][0] == "include") {
+            rederect.push_back(end[i][1]);
+        } else if (end[i][0] == "ipv4") {
+            ips.push_back(end[i][1]);
+        }
+    }
+
+    ares_free_data(txt_out);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <domain>" << std::endl;
+        return 1;
+    }
+
+    const char* domain = argv[1];
+
+    ares_channel channel;
+    int status = ares_library_init(ARES_LIB_INIT_ALL);
+    if (status != ARES_SUCCESS) {
+        std::cerr << "Failed to initialize c-ares library: " << ares_strerror(status) << std::endl;
+        return 1;
+    }
+
+    status = ares_init(&channel);
+    if (status != ARES_SUCCESS) {
+        std::cerr << "Failed to initialize c-ares channel: " << ares_strerror(status) << std::endl;
+        ares_library_cleanup();
+        return 1;
+    }
+
+    // Perform the DNS query for TXT records
+    ares_query(channel, domain, ns_c_in, ns_t_txt, callback, nullptr);
+
+    // Wait for the query to complete
+    // first request
+    for (;;) {
+        fd_set read_fds, write_fds;
+        int nfds;
+        struct timeval* tvp, tv;
+
+        FD_ZERO(&read_fds);
+        FD_ZERO(&write_fds);
+        nfds = ares_fds(channel, &read_fds, &write_fds);
+        if (nfds == 0) {
+            break;
+        }
+
+        tvp = ares_timeout(channel, nullptr, &tv);
+        select(nfds, &read_fds, &write_fds, nullptr, tvp);
+        ares_process(channel, &read_fds, &write_fds);
+    }
+
+    while (rederect.size() > 0) {
+        const char* tmp = rederect[0].c_str();
+        ares_query(channel, tmp, ns_c_in, ns_t_txt, callback, nullptr);
+        for (;;) {
+            fd_set read_fds, write_fds;
+            int nfds;
+            struct timeval* tvp, tv;
+
+            FD_ZERO(&read_fds);
+            FD_ZERO(&write_fds);
+            nfds = ares_fds(channel, &read_fds, &write_fds);
+            if (nfds == 0) {
+                break;
+            }
+
+            tvp = ares_timeout(channel, nullptr, &tv);
+            select(nfds, &read_fds, &write_fds, nullptr, tvp);
+            ares_process(channel, &read_fds, &write_fds);
+        }
+        rederect.erase(rederect.begin());
+    }
+
+    for (int i = 0; i < ips.size(); i++){
+        std::cout << ips[i] << " ";
+    }
+
+    ares_destroy(channel);
+    ares_library_cleanup();
+
+    return 0;
+}
--- dns.cpp
+++ dns.cpp
@@ -83,7 +83,15 @@ void query_ip_address(const std::string& domain, bool use_custom_dns = false, co
         query_length = res_query(domain.c_str(), C_IN, ns_t_aaaa, query_buffer, sizeof(query_buffer));
     else if (type == "cname")
         query_length = res_query(domain.c_str(), C_IN, ns_t_cname, query_buffer, sizeof(query_buffer));
-    
+    else if (type == "caa")
+        query_length = res_query(domain.c_str(), C_IN, ns_t_caa, query_buffer, sizeof(query_buffer));
+    else if (type == "ns")
+        query_length = res_query(domain.c_str(), C_IN, ns_t_ns, query_buffer, sizeof(query_buffer));
+    else if (type == "srv")
+        query_length = res_query(domain.c_str(), C_IN, ns_t_srv, query_buffer, sizeof(query_buffer));
+    else if (type == "txt")
+        query_length = res_query(domain.c_str(), C_IN, ns_t_txt, query_buffer, sizeof(query_buffer));
+
     if (query_length < 0) {
         std::cerr << "DNS query failed" << std::endl;
         return;
@@ -102,17 +110,19 @@ void query_ip_address(const std::string& domain, bool use_custom_dns = false, co
     for (int i = 0; i < answer_count; ++i) {
         ns_rr rr;
         if (ns_parserr(&handle, ns_s_an, i, &rr) == 0) {
-            const unsigned char* rdata = ns_rr_rdata(rr);
             if (ns_rr_type(rr) == ns_t_a) {
+                const unsigned char* rdata = ns_rr_rdata(rr);
                 char ip_address[INET_ADDRSTRLEN];
                 inet_ntop(AF_INET, rdata, ip_address, sizeof(ip_address));
                 std::cout << "  A (IPv4): " << ip_address << std::endl;
             } else if (ns_rr_type(rr) == ns_t_mx) {
+                const unsigned char* rdata = ns_rr_rdata(rr);
                 uint16_t preference = ntohs(*(uint16_t*)rdata);
                 char exchange[NS_MAXDNAME];
                 dn_expand(ns_msg_base(handle), ns_msg_end(handle), rdata + sizeof(uint16_t), exchange, sizeof(exchange));
                 std::cout << "  MX: " << exchange << " (Preference: " << preference << ")" << std::endl;
             } else if (ns_rr_type(rr) == ns_t_cname) {
+                const unsigned char* rdata = ns_rr_rdata(rr);
                 char cname[NS_MAXDNAME];
                 dn_expand(ns_msg_base(handle), ns_msg_end(handle), rdata, cname, sizeof(cname));
                 std::cout << "  CNAME: " << cname << std::endl;
@@ -122,6 +132,7 @@ void query_ip_address(const std::string& domain, bool use_custom_dns = false, co
                 dn_expand(ns_msg_base(handle), ns_msg_end(handle), rdata, name, sizeof(name));
                 std::cout << "  Type: " << ns_rr_type(rr) << ", Data: " << name << std::endl;
             } else if (ns_rr_type(rr) == ns_t_aaaa) {
+                const unsigned char* rdata = ns_rr_rdata(rr);
                 std::cout << "  AAAA (IPv6): ";
                 for (int j = 0; j < ns_rr_rdlen(rr); ++j) {
                     std::cout << std::hex << static_cast<int>(rdata[j]);
@@ -134,7 +145,33 @@ void query_ip_address(const std::string& domain, bool use_custom_dns = false, co
                 const unsigned char* rdata = ns_rr_rdata(rr);
                 char cname[NS_MAXDNAME];
                 dn_expand(ns_msg_base(handle), ns_msg_end(handle), rdata, cname, sizeof(cname));
-                std::cout << "  " << cname << std::endl;
+                std::cout << "  CNAME: " << cname << std::endl;
+            } else if (ns_rr_type(rr) == ns_t_caa) {
+                const unsigned char* rdata = ns_rr_rdata(rr);
+                std::cout << "  Flags: " << static_cast<int>(rdata[0]) << ", ";
+                unsigned char tag_len = rdata[1];
+                std::cout << "Tag: ";
+                for (int j = 2; j < 2 + tag_len; ++j) {
+                    std::cout << rdata[j];
+                }
+                std::cout << ", Value: ";
+                for (int j = 2 + tag_len; j < ns_rr_rdlen(rr); ++j) {
+                    std::cout << rdata[j];
+                }
+                std::cout << std::endl;
+            } else if (ns_rr_type(rr) == ns_t_ns) {
+                const unsigned char* rdata = ns_rr_rdata(rr);
+                char nsname[NS_MAXDNAME];
+                if (dn_expand(ns_msg_base(handle), ns_msg_end(handle), rdata, nsname, sizeof(nsname)) >= 0) {
+                    std::cout << "  NS: " << nsname << std::endl;
+                } else {
+                    std::cerr << "Failed to expand domain name" << std::endl;
+                }
+            } else if (ns_rr_type(rr) == ns_t_txt) {
+                const unsigned char* rdata = ns_rr_rdata(rr);
+                int txt_len = rdata[0];
+                std::string txt_record(reinterpret_cast<const char*>(rdata + 1), txt_len);
+                std::cout << "  TXT: " << txt_record << std::endl;
             } else {
                 std::cout << "  Unknown record type" << std::endl;
             }
